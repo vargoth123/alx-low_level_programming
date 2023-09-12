e98c8092a232caf04a96412684e083ca68e17621
vargoth123 <hassan.mustaphi@gmail.com>
2023-09-12 14:23:33 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,9 @@
 #include "main.h"
-
 /**
  * main - main function
  * Return: always 0
  */
-
 int main(void)
-
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,7 +14,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-			
 return (0);
-
 }
