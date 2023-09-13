22f6e55f4e43deb64bb6597109b339d225f0e677
vargoth123 <hassan.mustaphi@gmail.com>
2023-09-12 18:08:54 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,12 +2,11 @@
 
 /**
  * print_sign - Prints the sign of a number
- * @c: The nimber to be checked
+ * @n: The nimber to be checked
  * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 
 int print_sign(int n)
-
 {
 	if (n > 0)
 	{
