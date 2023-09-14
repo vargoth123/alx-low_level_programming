6711b6130455738ad3f76cc4517d7e780f3c3aab
vargoth123 <hassan.mustaphi@gmail.com>
2023-09-13 19:10:23 -0700

    'correction task 14'

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - main function
@@ -10,11 +12,11 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0 ; j <= 99 ; i++)
+	for (i = 0 ; j <= 98 ; i++)
 	{
-		for (j = 0 ; j <= 99 ; j++)
+		for (j = i + 1 ; j <= 99 ; j++)
 		{
-			if (i < j && i != j)
+			if (i < j ; && i != j)
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
